eeaa0e0c9c68aa64ad1d5a00fc3c09edc7ecfa13
anjdydhody <a01066444534@gmail.com>
2024-08-13 10:28:42 +0900

    first commit

---
+++ src/main/kotlin/sieun/chat/ServletInitializer.kt
@@ -0,0 +1,12 @@
+package sieun.chat
+
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
+
+class ServletInitializer : SpringBootServletInitializer() {
+
+	override fun configure(application: SpringApplicationBuilder): SpringApplicationBuilder {
+		return application.sources(ChatApplication::class.java)
+	}
+
+}
